5a0df392cd0634cc4bbb8dda6e9aa79a592e64c4
Danh Huynh <huynhthanhdanh9a4@gmail.com>
2024-10-26 15:11:53 +0700

    add payment

--- src/main/java/com/example/SGUCharity_Project/Controller/Payment_controller.java
+++ src/main/java/com/example/SGUCharity_Project/Controller/Payment_controller.java
@@ -1,6 +1,8 @@
 package com.example.SGUCharity_Project.Controller;
 
 import com.example.SGUCharity_Project.Config.VNPayService;
+import com.example.SGUCharity_Project.Model.Payment_model;
+import com.example.SGUCharity_Project.Repository.Payment_Repo;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
@@ -8,13 +10,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDateTime;
+
 @org.springframework.stereotype.Controller
 public class Payment_controller {
     @Autowired
     private VNPayService vnPayService;
 
+    @Autowired
+    private Payment_Repo paymentRepo;
 
-    @GetMapping("")
+
+    @GetMapping("/thanh-toan")
     public String home(){
         return "payment/payment_user";
     }
@@ -30,12 +37,22 @@ public class Payment_controller {
 
     @GetMapping("/vnpay-payment")
     public String GetMapping(HttpServletRequest request, Model model){
-        int paymentStatus =vnPayService.orderReturn(request);
+        int paymentStatus = vnPayService.orderReturn(request);
 
         String orderInfo = request.getParameter("vnp_OrderInfo");
         String paymentTime = request.getParameter("vnp_PayDate");
         String transactionId = request.getParameter("vnp_TransactionNo");
         String totalPrice = request.getParameter("vnp_Amount");
+
+        // Lưu thông tin thanh toán vào cơ sở dữ liệu
+        Payment_model payment = new Payment_model();
+        payment.setOrderId(orderInfo);
+        payment.setTotalPrice(totalPrice);
+        payment.setTransactionId(transactionId);
+        payment.setPaymentTime(LocalDateTime.now()); // Chuyển paymentTime từ String sang LocalDateTime nếu cần
+        payment.setPaymentStatus(paymentStatus);
+
+        paymentRepo.save(payment);
 
         model.addAttribute("orderId", orderInfo);
         model.addAttribute("totalPrice", totalPrice);
