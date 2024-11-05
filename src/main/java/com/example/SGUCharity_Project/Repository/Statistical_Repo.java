4577e8c7906da981a2c674dcd5a76f61868c2607
TestNG <huynhthanhdanh9a4@gmail.com>
2024-11-05 10:17:32 +0700

    StatisticalManagement

--- src/main/java/com/example/SGUCharity_Project/Repository/Statistical_Repo.java
+++ src/main/java/com/example/SGUCharity_Project/Repository/Statistical_Repo.java
@@ -1,2 +1,8 @@
-package com.example.SGUCharity_Project.Repository;public interface Statistical_Repo {
+package com.example.SGUCharity_Project.Repository;
+
+import com.example.SGUCharity_Project.Model.Payment_model;
+import com.example.SGUCharity_Project.Model.Statistical_model;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Statistical_Repo extends JpaRepository<Statistical_model, Long> {
 }
