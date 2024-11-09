a34ca76aa80a31d352659f34f1a0e420b71f4785
Danh Huynh <huynhthanhdanh9a4@gmail.com>
2024-11-09 14:10:21 +0700

    fix service

--- src/main/java/com/example/SGUCharity_Project/Model/Service_model.java
+++ src/main/java/com/example/SGUCharity_Project/Model/Service_model.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.SGUCharity_Project.Model;
 
 import jakarta.persistence.Entity;
@@ -22,28 +21,4 @@ public class Service_model {
     private String subtitle_service;
     private String urlartical_service;
 }
-=======
-//package com.example.SGUCharity_Project.Model;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//@Entity
-//@Data
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class ServiceOperation_model {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//    private String img_thumbnail;
-//    private String title_service;
-//    private String subtitle_service;
-//    private String urlartical_service;
-//}
->>>>>>> 7c34825e82cb50e2ad0270df27412ee50eed7a1f
+
